afe842be0ee3edd162e7379b4a473c716a47d8fe
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-26 12:21:32 +0530

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "${var.name}-SA"
+  name                     = "${var.name}storageaccount"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
