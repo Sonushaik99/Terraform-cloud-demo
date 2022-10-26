6a66f0ff8fca762349ca9cc7008b9cd1343706f8
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-26 13:04:37 +0530

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,11 +1,11 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "${var.name}storageaccount"
+  name                     = "${var.name}storage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    owner = "javeed"
+    owner = "admin"
   }
 }
